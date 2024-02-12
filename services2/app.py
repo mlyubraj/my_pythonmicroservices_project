c17e71bdf3fb979100ef66a9283d6a7daf86bf29
Manjay <mlyubraj@gmail.com>
2024-02-13 00:15:19 +0530

    Hello microservices

---
+++ services2/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+
+    return "Hello from Microservice 1 service1!"
+
+if __name__ == '__main__':
+
+    app.run(debug=True, port=5000)
+
